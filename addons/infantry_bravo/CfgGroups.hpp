cd9bbf774df46c4f1b047932f86ae9506b0f8be6
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-05-20 09:24:24 +0200

    Rewritten the soldier modules and internal structure (Squash)

---
+++ addons/infantry_bravo/CfgGroups.hpp
@@ -0,0 +1,11 @@
+class CfgGroups {
+    class West {
+        class cav_blu_usa_f {
+            class Cav_infantry_bravo_squads_f {
+                name = CSTRING(Cav_infantry_bravo_squads_f);
+                INFANTRY_GROUP_9(Cav_B_B_Group_Mechnaized_F,Cav_B_B_SquadLeader_F,Cav_B_B_FireTeamLeader_F,Cav_B_B_AutomaticRifleman_F,Cav_B_B_Grenadier_F,Cav_B_B_Rifleman_F,Cav_B_B_FireTeamLeader_F,Cav_B_B_AutomaticRifleman_F,Cav_B_B_Grenadier_F,Cav_B_B_CombatLifeSaver_F);
+                INFANTRY_TEAM_IFVCREW_2(Cav_B_B_Group_Mechnaized_Crew_F,Cav_B_B_IFV_Driver_F,Cav_B_B_IFV_Gunner_F);
+            };
+        };
+    };
+};
